0ec567438766da8ac5c4b22c12817084f56597f9
Shareef Dabdoub <shareef@dabdoub.net>
2010-03-11 02:16:01 +0000

    Moved specific clustering-related methods to their own modules.

--- cluster/methods.py
+++ cluster/methods.py
@@ -7,23 +7,26 @@ C{cluster()} method so that a consistent return can be maintained.
 @organization: The Ohio State University
 @organization: Nationwide Children's Hospital
 """
+from bakker_schut import bakker_kMeans
+from kmeans import kmeans
 
-import numpy
-from scipy.cluster.vq import whiten, vq, kmeans, kmeans2
-import Pycluster as pc
-import random
 import wx
 
-__all__ = ['cluster','kmeans','kinit','ID_KMEANS']
-
-#TODO: consider assigning these from a generator function starting well
-#      outside the range of the beginning of the wx.NewId() range (100)
-#      so as not to depend on the wx method for generating reliably static
-#      method IDs.
 ID_KMEANS = wx.NewId()
+ID_BAKKER_SCHUT = wx.NewId()
 ID_AUTOCLASS = wx.NewId()
 
 methods = {}
+methods[ID_KMEANS] = (ID_KMEANS, 'k-means', 
+                      'Cluster the data using the k-means algorithm', 
+                      kmeans, False)
+#methods[ID_BAKKER_SCHUT] = (ID_BAKKER_SCHUT, 'k-means', 
+#                            'Cluster the data using the k-means algorithm', 
+#                            bakker_kMeans, False)
+#methods[ID_AUTOCLASS] = (ID_AUTOCLASS, 'AutoClass', 
+#                         'Cluster the data using the AutoClass algorithm', 
+#                         None, False)
+
 
 def addPluginMethod(descriptor):
     """
@@ -82,61 +85,4 @@ def cluster(clusterType, data, **kwargs):
     method = methods[clusterType][3]
     return method(data, **kwargs)
 
-def kmeans(data, **kwargs):
-    """
-    Perform k-means clustering on unstructured N-dimensional data.
-    
-    @type data: array
-    @param data: The data to be clustered
-    @type kwargs: dict
-    @param kwargs: The following args are accepted:
-        - k: The number of clusters to form (returned number of clusters may be less than k).
-        - npasses: The number of times the k-means clustering algorithm is performed, 
-        each time with a different (random) initial condition.
-        - method: describes how the center of a cluster is found: 
-            - method=='a': arithmetic mean.
-            - method=='m': median.
-        - initialCenters: a set of points that should be used as the initial
-                          cluster centers
-            
-    @rtype: tuple
-    @return: A list where each element indicates the cluster membership of the 
-        corresponding index in the original data and a message string
-    """
-    k = 1
-    npasses = 1
-    method = 'a'
-    initialCenters = None
-    smartCenters = False
-    msg = ''
-    
-    if 'numClusters' in kwargs.keys():
-        k = int(kwargs['numClusters'])
-    if 'npasses' in kwargs.keys():
-        npasses = int(kwargs['npasses'])
-    if 'method' in kwargs.keys():
-        method = kwargs['method']
-    if 'initialCenters' in kwargs.keys():
-        initialCenters = kwargs['initialCenters']
-    if 'smartCenters' in kwargs.keys():
-        smartCenters = kwargs['smartCenters']
-        
-    if not initialCenters:
-        (clusterIDs, err, nOpt) = pc.kcluster(data, k, npass=npasses, method=method)
-        msg = "Number of rounds optimal solution was found: %i" % nOpt
-    else:
-        print "Using manually chosen centers:\n", initialCenters
-        (centroids, clusterIDs) = kmeans2(data, numpy.array(initialCenters), minit='matrix')
-        print "Final centroids: ", centroids
-    
-    return clusterIDs, msg
-
-
-
-methods[ID_KMEANS] = (ID_KMEANS, 'k-means', 
-                      'Cluster the data using the k-means algorithm', 
-                      kmeans, False)
-#methods[ID_AUTOCLASS] = (ID_AUTOCLASS, 'AutoClass', 
-#                         'Cluster the data using the AutoClass algorithm', 
-#                         None, False)
 
