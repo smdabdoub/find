cf92e8115ae1f1d16492a55de553f98d3903c539
Shareef Dabdoub <shareef@dabdoub.net>
2010-03-11 02:16:31 +0000

    Moved to its own module to clean up the code organization.

---
+++ facsGUI/cluster/kmeans.py
@@ -0,0 +1,56 @@
+'''
+Created on Mar 1, 2010
+
+@author: shareef
+'''
+import numpy as np
+from scipy.cluster.vq import whiten, vq, kmeans, kmeans2
+import Pycluster as pc
+
+def kmeans(data, **kwargs):
+    """
+    Perform k-means clustering on unstructured N-dimensional data.
+    
+    @type data: array
+    @param data: The data to be clustered
+    @type kwargs: dict
+    @param kwargs: The following args are accepted:
+        - k: The number of clusters to form (returned number of clusters may be less than k).
+        - npasses: The number of times the k-means clustering algorithm is performed, 
+        each time with a different (random) initial condition.
+        - method: describes how the center of a cluster is found: 
+            - method=='a': arithmetic mean.
+            - method=='m': median.
+        - initialCenters: a set of points that should be used as the initial
+                          cluster centers
+            
+    @rtype: tuple
+    @return: A list where each element indicates the cluster membership of the 
+        corresponding index in the original data and a message string
+    """
+    k = 1
+    npasses = 1
+    method = 'a'
+    initialCenters = None
+    smartCenters = False
+    msg = ''
+    
+    if 'numClusters' in kwargs.keys():
+        k = int(kwargs['numClusters'])
+    if 'npasses' in kwargs.keys():
+        npasses = int(kwargs['npasses'])
+    if 'method' in kwargs.keys():
+        method = kwargs['method']
+    if 'initialCenters' in kwargs.keys():
+        initialCenters = kwargs['initialCenters']
+    if 'smartCenters' in kwargs.keys():
+        smartCenters = kwargs['smartCenters']
+        
+    if not initialCenters:
+        (clusterIDs, err, nOpt) = pc.kcluster(data, k, npass=npasses, method=method)
+        msg = "Number of rounds optimal solution was found: %i" % nOpt
+    else:
+        print "Using manually chosen centers:\n", initialCenters
+        (centroids, clusterIDs) = kmeans2(data, np.array(initialCenters), minit='matrix')
+    
+    return clusterIDs, msg
