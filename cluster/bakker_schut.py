a24ae07d3358f4d1653f201ccab8a4c6e58793c3
Shareef Dabdoub <shareef@dabdoub.net>
2010-04-27 12:46:11 +0000

    Still in the process of making BS kmeans work. Main focus is the final step: merging. Added a method to update the cluster distance matrix for speed (still testing). Moved to storing clusters as arrays in a dict keyed on cluster ID to deal with constantly adding "new" clusters by merging.

--- cluster/bakker_schut.py
+++ cluster/bakker_schut.py
@@ -22,14 +22,7 @@ def bakker_kMeans(data, **kwargs):
     @param data: The data to be clustered
     @type kwargs: dict
     @param kwargs: The following args are accepted:
-        - k: The number of clusters to form (returned number of clusters may be less than k).
-        - npasses: The number of times the k-means clustering algorithm is performed, 
-        each time with a different (random) initial condition.
-        - method: describes how the center of a cluster is found: 
-            - method=='a': arithmetic mean.
-            - method=='m': median.
-        - initialCenters: a set of points that should be used as the initial
-                          cluster centers
+        - numClusters: The number of clusters to form
             
     @rtype: tuple
     @return: A list where each element indicates the cluster membership of the 
@@ -53,28 +46,34 @@ def bakker_kMeans(data, **kwargs):
     _, ids = kmeans2(logData, np.array(centers), minit='matrix')
     
     # Merge clusters w/special comparison metric until user cluster # achieved
-    clusters = util.separate(logData, ids)
+    clusters = util.separate(data, ids)
     final = merge(k, ids, clusters)
     
     return final, msg
-
-
-def seedCenters(k, data):
-    """
-    """
-    centers = [data[0]]
-    
-    for d in data:
-        pass
     
 
-def merge(limit, ids, clusters, dist=None):
+def merge(limit, ids, clusters, dist=None, minpair=None, newID=None):
     """
     Take a set of clusters and iteratively merge them together based on some 
-    distance metric until the lower limit is reached
+    distance metric until the lower limit is reached.
+    
+    :@type limit: int
+    :@param limit: The target number of clusters.
+    :@type ids: list or array
+    :@param ids: A list containing the cluster ID of each row of data.
+    :@type clusters: list
+    :@param clusters: A list of arrays, each containing the 
     """
+    if (type(clusters) is not dict):
+        tmp = {}
+        for i, cluster in enumerate(clusters):
+            tmp[i] = cluster
+        clusters = tmp
+        
+    
     if len(clusters) <= limit:
-        saved = dict(zip(np.unique(ids), range(len(clusters))))
+        unique = np.unique(ids)
+        saved = dict(zip(unique, range(len(unique))))
         # reassign cluster ids to a contiguous range
         for i, id in enumerate(ids):
             ids[i] = saved[id]
@@ -84,20 +83,23 @@ def merge(limit, ids, clusters, dist=None):
     # calculate distance matrix
     if (dist is None):
         dist, minpair = distMatrix(clusters)
+    else:
+        dist, minpair = updateDistMatrix(clusters, dist, minpair, newID)
     
     # combine the two most similar clusters
     merged = dh.mergeData(minpair, clusters)
     
+    # merge the minpair clusters and reassign their IDs to the new
     for i in minpair:
         del clusters[i]
-    clusters.append(merged)
-    newID = len(clusters) - 1
+    newID = np.max(ids) + 1
+    clusters[newID] = merged
     
     for i, id in enumerate(ids):
         if id in minpair:
             ids[i] = newID 
     
-    merge(limit, clusters, ids)
+    return merge(limit, clusters, ids, dist, minpair, newID)
     
     
         
@@ -108,19 +110,72 @@ def distMatrix(clusters):
     """
     Calculate the all-pairs distance matrix between the given clusters
     
-    @type clusters: list
-    @param clusters:  The list of clusters to calculate the all-pairs distance matrix
+    @type clusters: dict
+    @param clusters:  The list of clusters to calculate the all-pairs distance 
+                      matrix, keyed on cluster ID. The new cluster is assumed to be the last element. 
     @rtype: tuple
     @return: The all-pairs distance matrix and the pair with the overall minimum distance 
     """
-    n = len(clusters)
-    min = -1; minpair = None
-    dist = np.zeros((n, n))
-    for i in range(n):
-        for j in range(i+1, n):
-            dist[i,j] = util.nonSymmetricClusterDistance(clusters[i], clusters[j])
-            if min < 0 or dist[i,j] < min:
-                min = dist[i,j]
+    min = None; minpair = None
+    dist = {}
+    ids = np.sort(clusters.keys())
+    for i in ids:
+        dist[i] = {}
+        for j in ids[i+1:ids.shape[0]]:
+            dist[i][j] = util.nonSymmetricClusterDistance(clusters[i], clusters[j])
+            if (min is None) or (dist[i][j] < min):
+                min = dist[i][j]
                 minpair = (i,j)
     
     return dist, minpair
+
+
+def updateDistMatrix(clusters, matrix, remIDs, newID):
+    """
+    Selectively update the distance calculations by deleting the removed 
+    clusters and adding calculations for the replacement cluster 
+    
+    :@type clusters: dict
+    :@param clusters: The data split into clusters stored in numpy arrays, 
+                      keyed on ID.
+    :@type matrix: dict
+    :@param matrix: A dict containing one dict for each cluster, keyed on the 
+                    cluster id with the distance as the value.
+    :@type remIDs: tuple
+    :@param remIDs: The ids of the removed clusters.
+    :@type newID: int
+    :@param newID: The id of the replacement cluster.
+    """
+    for i in np.sort(clusters.keys()):
+        # remove the old cluster distance calculations
+        if not i in remIDs:
+            for j in remIDs:
+                if j in matrix[i]:
+                    del matrix[i][j]
+            # calculate distances to the new cluster
+            matrix[i][newID] = util.nonSymmetricClusterDistance(clusters[i], clusters[newID])
+        else:
+            del matrix[i]
+    
+    # calculate the minpair: 
+    # inner min finds the minpairs for each row, outer min finds overall minpair 
+    minpair = min([(i,min(matrix[i], key=lambda z:matrix[i].get(z))) for i in matrix], 
+                  key=lambda p: matrix.get(p[0]).get(p[1]))
+    
+    return matrix, minpair
+
+
+
+
+def main():
+    from data.io import loadDataFile
+    f = loadDataFile('/data/537-Filter-Spin_MMC-Top.fcs')
+    data = f[1]
+    c = bakker_kMeans(data, numClusters=3)
+    print c
+    
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
