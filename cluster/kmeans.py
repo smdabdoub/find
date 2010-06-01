40306d0136ddf4fa4a3018f57ab81037f645f4d0
Shareef Dabdoub <shareef@dabdoub.net>
2010-06-01 11:48:32 +0000

    moved the kmeans options dialog here.

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -56,3 +56,152 @@ def kmeans(data, **kwargs):
         (centroids, clusterIDs) = kmeans2(logData, np.array(initialCenters), minit='matrix')
     
     return clusterIDs, msg
+
+
+
+#----------------------------------
+# K-MEANS DIALOG CLASS
+#----------------------------------
+from cluster.util import ClusterOptionsDialog
+import wx
+
+class KMeansDialog(ClusterOptionsDialog):
+    """
+    Provide an options dialog for the k-means clustering algorithm 
+    """
+    
+    
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, 'K-Means Options', size=(350, 300))
+        self.CenterOnParent()
+        
+        # create form controls
+        self.txtNumClusters   = wx.TextCtrl(self, wx.ID_ANY, '3', size=(50,20))
+        self.cbxMethod        = wx.ComboBox(self, wx.ID_ANY, 'Mean', (-1,-1), (160, -1), ['Mean','Median'], wx.CB_READONLY)
+        self.txtNumPasses     = wx.TextCtrl(self, wx.ID_ANY, '5', size=(50,20))
+        self.chkInitialCenters = wx.CheckBox(self, wx.ID_ANY, 'Manually select centers?')
+        self.initialCenters = []
+        
+        self.chkInitialCenters.Bind(wx.EVT_CHECKBOX, self.chkInitialCenters_Click)
+        
+        # create a table of label-input controls
+        self.formSizer = wx.GridSizer(4, 2, vgap=20) #rows,cols,vgap,hgap
+        self.formSizer.Add(wx.StaticText(self, -1, 'Number of clusters:', (20, 10)), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.txtNumClusters, 1)
+        self.formSizer.Add(wx.StaticText(self, -1, 'K-Means method:', (20, 5)), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.cbxMethod, 1, wx.EXPAND)
+        self.formSizer.Add(wx.StaticText(self, -1, 'Number of Passes:', (20, 10)), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.txtNumPasses, 1)
+        
+        # create the button row
+        self.buttonSizer = self.CreateButtonSizer(wx.OK | wx.CANCEL)
+        self.buttonSizer.AffirmativeButton.Bind(wx.EVT_BUTTON, self.cmdOK_Click)
+        #self.buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+        #self.cmdOK = wx.Button(self, wx.ID_OK)
+        #self.cmdCancel = wx.Button(self, wx.ID_CANCEL)
+        #self.cmdOK.Bind(wx.EVT_BUTTON, self.cmdOK_Click)
+        #self.buttonSizer.Add(self.cmdOK, 0, wx.EXPAND)
+        #self.buttonSizer.Add(self.cmdCancel, 0, wx.EXPAND)
+        
+        # main sizer
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.formSizer, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 20)
+        self.sizer.Add(self.chkInitialCenters, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 20)
+        self.sizer.Add(self.getApplySizer(self), 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 20)
+        self.sizer.Add(self.buttonSizer, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, 20)
+        self.SetSizer(self.sizer)
+        
+        
+    def cmdOK_Click(self,event):
+        if self.chkInitialCenters.IsChecked():
+            from cluster.dialogs import CenterChooserDialog
+            dlg = CenterChooserDialog(self)
+            if dlg.ShowModal() == wx.ID_OK:
+                self.initialCenters = dlg.GetCenters()
+            dlg.Destroy()
+        event.Skip()
+            
+    def chkInitialCenters_Click(self, event):
+        if (event.Selection):
+            self.txtNumPasses.Enable(False)
+            self.txtNumPasses.Value = '0'
+        else:
+            self.txtNumPasses.Enable(True)
+    
+    
+    def getMethodArgs(self):
+        """
+        Gather all options specified in the dialog.
+        
+        @rtype: dict
+        @return: A dictionary of algorithm options.
+        """
+        options = {}
+        options['numClusters'] = self._getNumClusters()
+        options['method'] = self._getMethod()
+        options['numPasses'] = self._getNumPasses()
+        options['initialCenters'] = self._getInitialCenters()
+        return options
+    
+    def getStrMethodArgs(self):
+        """
+        Define an equivalency between the variable form of algorithm options 
+        and the full string representation of the options.
+        
+        @rtype: tuple
+        @return: A tuple containing:
+            - A dictionary equating the short form of method option names
+            with the full string descriptions.
+            - A dictionary containing translations for any argument values
+            that are not easily understandable
+        """
+        options = {}
+        options['numClusters'] = 'Number of Clusters'
+        options['method'] = 'Method'
+        options['numPasses'] = 'Number of Passes'
+        options['initialCenters'] = 'Manually chosen centers'
+        values = {}
+        values['method'] = {'a':'Mean', 'm':'Median'}
+        return (options, values)
+        
+        
+    def _getNumClusters(self):
+        """
+        Retrieve the specified cluster target for the algorithm.
+        
+        @rtype: int
+        @return: The number of clusters to find.
+        """
+        return int(self.txtNumClusters.GetValue())
+    
+    def _getMethod(self):
+        """
+        Retrieve the specified method for determining the cluster center.
+        
+        @rtype: char
+        @return: The method by which the cluster center is determined.
+        """
+        if (self.cbxMethod.GetValue() == 'Mean'):
+            return 'a'
+        if (self.cbxMethod.GetValue() == 'Median'):
+            return 'm'
+    
+    def _getNumPasses(self):
+        """
+        Retrieve the specified number of times the k-means algorithm should
+        be run.
+        
+        @rtype: int
+        @return: The number of passes the algorithm should perform.
+        """
+        return int(self.txtNumPasses.GetValue())
+    
+    def _getInitialCenters(self):
+        """
+        Retrieve the user-specified points to be used as initial centers for the 
+        k-means algorithm
+        
+        @rtype: list
+        @return: A list of 2D points
+        """
+        return self.initialCenters
