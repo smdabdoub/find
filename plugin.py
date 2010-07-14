7bbd298e9108df60193ceab1628476838e0d45cc
Shareef Dabdoub <shareef@dabdoub.net>
2010-07-14 18:32:15 +0000

    Added handling for missing plugin directories.

--- plugin.py
+++ plugin.py
@@ -3,10 +3,12 @@ Created on Jul 30, 2009
 
 @author: shareef
 '''
+import error
+
 import os.path
 import sys
 
-pluginTypes = ['cluster', 'analysis', 'transforms', 'graph', 'IO']
+pluginTypes = ['analysis', 'cluster', 'graph', 'IO', 'transforms']
 loaded = dict([(type_, []) for type_ in pluginTypes])
 
 def discoverPlugins():
@@ -14,8 +16,9 @@ def discoverPlugins():
     Adds the plugins directory to the system path and interrogates it for 
     available plugins in the folders named in the module global pluginTypes list.
     
-    @rtype: dict
-    @return: A dictionary of plugins keyed on their type.
+    :@raise error.PluginError: If the main plugin directory or any subdirectories are missing.
+    :@rtype: dict
+    :@return: A dictionary of plugins keyed on their type.
     """
     availablePlugins = {}
     pluginDir = 'plugins'
@@ -25,16 +28,31 @@ def discoverPlugins():
     if sys.platform == 'darwin':
         basePluginPath = sys.path[0].split('find.app')[0]
     
+    # make sure the plugins folder exists
+    if not os.path.exists(os.path.join(basePluginPath, pluginDir)):
+        raise error.PluginError("The \"plugins\" directory is missing, please place in the same folder as the FIND program file.")
+        return
+    
     if not basePluginPath in sys.path:
         sys.path.insert(0, basePluginPath)
     
     #TODO: look at recursive descent for module discovery
     # Accumulate available plugin modules
+    errMsg = []
     for type_ in pluginTypes:
         path = os.path.join(basePluginPath, pluginDir, type_)
-        availablePlugins[type_] = [fname[:-3] for fname in os.listdir(path) 
-                                              if fname.endswith(".py") and 
-                                               not fname.startswith("__init__")]
+        if not os.path.exists(path):
+            errMsg.append("The \"%s\" plugins directory is missing." % type_)
+        else:
+            availablePlugins[type_] = [fname[:-3] for fname in os.listdir(path) 
+                                                  if fname.endswith(".py") and 
+                                                   not fname.startswith("__init__")]
+    
+    # raise error with all missing plugin subdirectories
+    if len(errMsg) > 0:
+        raise error.PluginError('\n\t'.join(errMsg))
+        return
+    
     return availablePlugins
 
     
@@ -43,8 +61,8 @@ def importPlugins(plugins):
     Imports the supplied plugins, storing their names in the module global
     'loaded' dictionary.
     
-    @type plugins: dict
-    @var plugins: A dictionary of available plugin files for import, 
+    :@type plugins: dict
+    :@var plugins: A dictionary of available plugin files for import, 
                   keyed on plugin type.
     """
     global loaded
