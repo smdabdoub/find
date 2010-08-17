f58823cc4344c4df1ba303545552c2d6662f4857
Shareef Dabdoub <shareef@dabdoub.net>
2010-08-17 17:38:31 +0000

    Adding an options dialog for opening (and possibly saving) CSV files.

--- plugins/IO/csv.py
+++ plugins/IO/csv.py
@@ -6,6 +6,7 @@ from data.io import FILE_INPUT, FILE_OUTPUT
 from plugins.pluginbase import IOPlugin 
 
 from numpy import loadtxt
+import wx
 
 __all__ = ['register_csv']
 
@@ -30,12 +31,24 @@ class CSVPlugin(IOPlugin):
         @return: A tuple containing a list of column labels and numpy array 
             containing the actual FACS data.
         """
+        # display options dialog first
+        delim = ','
+        skiprows = 1
+        commentChar = '#'
+        dlg = CSVOptionsDialog()
+        if dlg.ShowModal() == wx.ID_OK:
+            delim = dlg.Delimiter
+            skiprows = dlg.HeaderLineNumber
+            commentChar = dlg.CommentCharacter
+        dlg.Destroy()
+        
         # Retrieve first line of column labels
         facsFile = open(self.filename,'r')
         labels = facsFile.readline().rstrip().replace('"','').split(',')
         facsFile.close()
+        
         # load actual data
-        data = loadtxt(self.filename, delimiter=',', skiprows=1)
+        data = loadtxt(self.filename, comments=commentChar, delimiter=delim, skiprows=skiprows)
         return (labels,data, {})
     
     def save(self, facsData):
@@ -50,7 +63,74 @@ class CSVPlugin(IOPlugin):
         
         
         
+
+class CSVOptionsDialog(wx.Dialog):
+    def __init__(self):
+        # form controls
+        self.txtHeaderLine = wx.TextCtrl(self, wx.ID_ANY, '1', size=(50,20))
+        self.txtCommentChar = wx.TextCtrl(self, wx.ID_ANY, '#', size=(50,20))
+        self.txtDelimiter = wx.TextCtrl(self, wx.ID_ANY, ',', size=(50,20))
         
+        # create a table of label-input controls
+        self.formSizer = wx.GridSizer(3, 2, vgap=20)
+        self.formSizer.Add(wx.StaticText(self, wx.ID_ANY, 'Header line number:'), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.txtHeaderLine, 1)
+        self.formSizer.Add(wx.StaticText(self, wx.ID_ANY, 'Comment character:'), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.txtCommentChar, 1)
+        self.formSizer.Add(wx.StaticText(self, wx.ID_ANY, 'Delimiter:'), 1, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.formSizer.Add(self.txtDelimiter, 1)
+        
+        # create the button row
+        self.buttonSizer = self.CreateButtonSizer(wx.OK | wx.CANCEL | wx.HELP)
+        self.buttonSizer.AffirmativeButton.Bind(wx.EVT_BUTTON, self.cmdOK_Click)
+        self.buttonSizer.HelpButton.Bind(wx.EVT_BUTTON, self.cmdHelp_Click)
+        
+        # main sizer
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.formSizer, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 20)
+        self.sizer.Add(self.buttonSizer, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, 20)
+        self.SetSizer(self.sizer)
+            
+    
+    @property
+    def CommentCharacter(self):
+        return self.txtCommentChar
+    
+    @property
+    def Delimiter(self):
+        return self.txtDelimiter
+    
+    @property
+    def HeaderLineNumber(self):
+        return int(self.txtHeaderLine)
+
+
+    def validate(self):
+        return []
+        
+   
+    #TODO: this should be generalized to a OptionsDialog class instead of just ClusterOptionsDialog
+    def cmdOK_click(self, event):
+        """
+        Call the form validation method and display any error messages.
+        """
+        msg = self.validate()
+        if len(msg) > 0:
+            dlg = wx.MessageDialog(None, '\n'.join(msg), 
+                                   'Invalid input', wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return
+        
+        event.Skip()
+    
+    
+    def cmdHelp_Click(self, event):
+        pass
+#        from display.help import HelpDialog
+#        HelpDialog(self, "k-means help", htmlfile="help/k-means_clustering.html").Show()
+
+
 def register_csv():
     return ('csv', CSVPlugin)
         
