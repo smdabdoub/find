e580a04876975061e7139b421fa8a68c33dd9b2c
Shareef Dabdoub <shareef@dabdoub.net>
2010-08-24 21:19:14 +0000

    Moved the CenterChooserDialog here.

--- find/cluster/kmeans.py
+++ find/cluster/kmeans.py
@@ -115,7 +115,6 @@ class KMeansDialog(ClusterOptionsDialog):
         
     def cmdOK_Click(self,event):
         if self.chkInitialCenters.IsChecked():
-            from cluster.dialogs import CenterChooserDialog
             dlg = CenterChooserDialog(self)
             if dlg.ShowModal() == wx.ID_OK:
                 self.initialCenters = dlg.GetCenters()
@@ -238,3 +237,125 @@ class KMeansDialog(ClusterOptionsDialog):
         @return: A list of 2D points
         """
         return self.initialCenters
+    
+    
+    
+from data.store import DataStore
+import display.customsizers as customsizers
+
+class CenterChooserDialog(wx.Dialog):
+    """
+    Provide scatterplot view of the data for choosing centers 
+    """
+    
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, 'Cluster Center Chooser', size=(550, 550))
+        self.CenterOnParent()
+        
+        self.centers = []
+        
+        # set up the plotting panel
+        self.dataPanel = CenterChooserPanel(self, self.OnClick)
+        (self.dataSelectors, self.selectorSizer) = customsizers.ChannelSelectorSizer(self)
+        
+        if DataStore.getCurrentIndex() is not None:
+            self.dataPanel.addData(DataStore.getCurrentIndex())
+            
+            # filter the list of labels to include only user-selected ones
+            selDims = DataStore.getCurrentDataSet().selDims
+            labels = [DataStore.getCurrentDataSet().labels[i] for i in selDims]
+            customsizers.populateSelectors(self.dataSelectors, labels, selDims)        
+            self.dataPanel.updateAxes((selDims[0],selDims[1]))
+        
+        # main sizer
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.dataPanel, True, wx.EXPAND | wx.BOTTOM | wx.TOP, -20)
+        self.sizer.AddSpacer(20)
+        self.sizer.Add(self.selectorSizer, False, wx.EXPAND | wx.TOP, 20)
+        self.sizer.Add(self.CreateButtonSizer(wx.OK | wx.CANCEL), False, wx.EXPAND)
+        
+        self.SetSizer(self.sizer)
+        self.SetBackgroundColour("white")
+        
+    
+    
+    def OnClick(self, event):
+        if (event.inaxes is not None):
+            self.AddChosenCenter((event.xdata, event.ydata))
+
+
+    def OnCBXClick(self, e):
+        """ 
+        Instructs the FacsPlotPanel instance to update the displayed axis 
+        based on the selection made in the axis selection region of the main frame.
+        """
+        cbxSelected = self._GetSelectedAxes()
+        self.dataPanel.updateAxes(cbxSelected)
+        
+    
+    def _GetSelectedAxes(self):
+        return [cbx.GetClientData(cbx.GetSelection()) for cbx in self.dataSelectors]
+    
+    
+    def AddChosenCenter(self, point):
+        cbxSelected = self._GetSelectedAxes()
+        dims = DataStore.getCurrentDataSet().labels
+        if (DataStore.getCurrentDataSet().selDims):
+            dims = DataStore.getCurrentDataSet().selDims
+        retPoint = [0 for i in dims] 
+        for i in range(len(cbxSelected)):
+            retPoint[cbxSelected[i]] = point[i]
+        
+        self.centers.append(retPoint)
+        self.dataPanel.addCenter(retPoint)
+    
+    def GetCenters(self):
+        return self.centers
+
+
+import data.view as dv
+import plot.methods as pmethods
+
+class CenterChooserPanel(dv.PlotPanel):
+    def __init__(self, parent, clickHandler, **kwargs):
+        # initiate plotter
+        dv.PlotPanel.__init__( self, parent, **kwargs )
+        self._setColor((255,255,255))
+        self.figure.canvas.mpl_connect('button_press_event', clickHandler)
+        
+        # private members
+        self.plot = None
+        self.centers = []
+        self.axes = (0, 1)
+        
+    def addData(self, dataIndex):
+        self.plot = dv.Subplot(1, dataIndex)
+        self.plot.mnp = '111'
+    
+    def addCenter(self, center):
+        self.centers.append(center)
+        self.draw()
+    
+    def updateAxes(self, axes):
+        self.axes = axes
+        self.draw()
+        
+        
+    def draw(self):
+        self.figure.clear()
+        # scatterplot data
+        pmethods.getMethod(self.plot.plotType)(self.plot, self.figure, self.axes)
+        
+        # draw centers
+        try:
+            c = np.asarray(self.centers)
+            self.plot.axes.plot(c[:, self.axes[0]], c[:, self.axes[1]],'o', ms=5, color='red')
+        except IndexError:
+            pass
+        
+        self.canvas.draw()
+        
+        
+ 
+        
+        
