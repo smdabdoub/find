ad3f9b5df3cb5ea315390a43e58d2b9fbaa4621e
Shareef Dabdoub <shareef@dabdoub.net>
2010-09-17 19:28:16 +0000

    Completely reworked the tree control to include support for Figure sets

--- display/view.py
+++ display/view.py
@@ -5,13 +5,18 @@ Created on Aug 23, 2010
 '''
 
 from cluster.util import clusteringInfo
-from data.store import FacsData, DataStore
+from data.store import FacsData, DataStore, FigureStore, Figure
+from data.view import switchFigures
 from display.dialogs import DataInfoDialog, EditNameDialog
-from display.contextmenus import TreePopupMenu
+from display.contextmenus import DataTreePopupMenu, FigureTreePopupMenu
 import cluster.methods
 import wx.lib.customtreectrl as CT
-
 import wx
+
+
+
+DATA_SET_ITEM = 0
+FIGURE_SET_ITEM = 1
 
 
 class FacsTreeCtrlPanel(wx.Panel):
@@ -20,12 +25,15 @@ class FacsTreeCtrlPanel(wx.Panel):
         wx.Panel.__init__(self, parent, -1, style=wx.WANTS_CHARS, size=(200,550))
         self.Bind(wx.EVT_SIZE, self.OnSize)
         self.tree = CT.CustomTreeCtrl(self, wx.NewId(), wx.DefaultPosition, (200,550), CT.TR_DEFAULT_STYLE)
+        self.dataTreeExpanded = False
+        self.figureTreeExpanded = False
 
         isz = (16,16)
         il = wx.ImageList(isz[0], isz[1])
         self.fldridx     = il.Add(wx.ArtProvider_GetBitmap(wx.ART_FOLDER,      wx.ART_OTHER, isz))
         self.fldropenidx = il.Add(wx.ArtProvider_GetBitmap(wx.ART_FILE_OPEN,   wx.ART_OTHER, isz))
         self.fileidx     = il.Add(wx.ArtProvider_GetBitmap(wx.ART_NORMAL_FILE, wx.ART_OTHER, isz))
+        self.figureicn   = il.Add(wx.ArtProvider_GetBitmap(wx.ART_NORMAL_FILE,  wx.ART_OTHER, isz))
 
         self.tree.SetImageList(il)
         self.il = il
@@ -45,17 +53,33 @@ class FacsTreeCtrlPanel(wx.Panel):
         """
         self.tree.DeleteAllItems()
         
-        self.root = self.tree.AddRoot("Data Sets")
+        self.root = self.tree.AddRoot("Project")
         self.tree.SetPyData(self.root, None)
         self.tree.SetItemImage(self.root, self.fldridx, wx.TreeItemIcon_Normal)
         self.tree.SetItemImage(self.root, self.fldropenidx, wx.TreeItemIcon_Expanded)
+        
+        
+        dataTree = self.tree.AppendItem(self.root, "Data Sets")
+        self.tree.SetPyData(dataTree, DATA_SET_ITEM)
+        self.tree.SetItemImage(dataTree, self.fldridx, wx.TreeItemIcon_Normal)
+        self.tree.SetItemImage(dataTree, self.fldropenidx, wx.TreeItemIcon_Expanded)
+        
+        figsTree = self.tree.AppendItem(self.root, "Figure Sets")
+        self.tree.SetPyData(figsTree, FIGURE_SET_ITEM)
+        self.tree.SetItemImage(figsTree, self.fldridx, wx.TreeItemIcon_Normal)
+        self.tree.SetItemImage(figsTree, self.fldropenidx, wx.TreeItemIcon_Expanded)
 
-        self.buildTree(self.root, DataStore.getData(), [])
+        # create tree
+        self.buildDataTree(dataTree, DataStore.getData(), [])
+        self.buildFigureTree(figsTree, FigureStore.getFigures())
         self.tree.Expand(self.root)
-            
+        if self.dataTreeExpanded:
+            self.tree.Expand(dataTree)
+        if self.figureTreeExpanded:
+            self.tree.Expand(figsTree)    
 
 
-    def buildTree(self, parent, dataDict, visited):
+    def buildDataTree(self, parent, dataDict, visited):
         for dIndex, fData in dataDict.iteritems():
             # add tree node for the FACS data set
             if fData.ID in visited:
@@ -63,7 +87,7 @@ class FacsTreeCtrlPanel(wx.Panel):
             child = self.tree.AppendItem(parent, fData.displayname)
             if (DataStore.getCurrentIndex() == dIndex):
                 self.tree.SetItemBold(child, True)
-            self.tree.SetPyData(child, dIndex)
+            self.tree.SetPyData(child, (DATA_SET_ITEM, dIndex))
             self.tree.SetItemImage(child, self.fldridx, wx.TreeItemIcon_Normal)
             self.tree.SetItemImage(child, self.fldropenidx, wx.TreeItemIcon_Expanded)
             
@@ -75,7 +99,7 @@ class FacsTreeCtrlPanel(wx.Panel):
                 if ((fData.selectedClustering == cIndex) and (fData.ID == DataStore.getCurrentIndex())):
                     self.tree.SetItemBold(clust, True)
                 toolTip = clusteringInfo(fData, cIndex)
-                self.tree.SetPyData(clust, (dIndex,cIndex,toolTip)) #(data index, cluster index, tooltip)
+                self.tree.SetPyData(clust, (DATA_SET_ITEM, dIndex, cIndex, toolTip)) #(data index, cluster index, tooltip)
                 self.tree.SetItemImage(clust, self.fldridx, wx.TreeItemIcon_Normal)
                 self.tree.SetItemImage(clust, self.fldropenidx, wx.TreeItemIcon_Expanded)
                 # set clustering options
@@ -89,12 +113,20 @@ class FacsTreeCtrlPanel(wx.Panel):
             
             # recursive call to build the tree for any children
             visited.append(fData.ID)
-            self.buildTree(child, dict([(i,DataStore.getData()[i]) for i in fData.children]), visited)
+            self.buildDataTree(child, dict([(i,DataStore.getData()[i]) for i in fData.children]), visited)
             
             if (fData.nodeExpanded and self.tree.GetChildrenCount(child) > 0):
                 self.tree.Expand(child)
 
 
+    def buildFigureTree(self, root, figures):
+        for id, fig in figures.iteritems():
+            child = self.tree.AppendItem(root, fig.name)
+            if (FigureStore.getSelectedIndex() == id):
+                self.tree.SetItemBold(child, True)
+            self.tree.SetPyData(child, (FIGURE_SET_ITEM, id))
+            self.tree.SetItemImage(child, self.figureicn, wx.TreeItemIcon_Normal)
+        
 
 
 
@@ -113,11 +145,27 @@ class FacsTreeCtrlPanel(wx.Panel):
     
     
     def getSanitizedItemSelectionData(self):
+        """
+        Retrieves the selected item data. If data is a tuple 
+        (i.e. data, clustering, or figure) it returns a 2- or 3-tuple
+        as appropriate.
+        
+        Data: 2-tuple - (DATA_SET_ITEM, data ID)
+        Clustering: 3-tuple - (DATA_SET_ITEM, data ID, cluster ID)
+        Figure: 2-tuple - (FIGURE_SET_ITEM, figure ID)
+        
+        @rtype: tuple
+        @return: A 2 or 3-tuple depending on the selected item data.
+        """
         data = self.getItemSelectionData()
-        if (isinstance(data,int)):
-            return (data, None)
+        
+        # data, clustering, or figure
         if (isinstance(data,tuple)):
-            return (data[0], data[1])
+            # clustering
+            if len(data) > 2:
+                return (data[0], data[1], data[2])
+            else:
+                return data
     
    
     def applyToSelection(self, dataFunc, clustFunc):
@@ -131,73 +179,125 @@ class FacsTreeCtrlPanel(wx.Panel):
         @param clustFunc: A function to apply to the clustering of the FacsData
             object selected.
         """
-        data = self.getItemSelectionData()
-        # determine if item is a data set or a clustering and select it
-        if (isinstance(data,int)):
-            dataFunc(data)
-        if (isinstance(data,tuple)):
-            clustFunc(DataStore.selectDataSet(data[0]), data[1])
+        data = self.getSanitizedItemSelectionData()
+        
+        if data is not None and data[0] is DATA_SET_ITEM:
+            # determine if item is a data set or a clustering and select it
+            if len(data) == 2:
+                dataFunc(data[1])
+            if len(data) == 3:
+                clustFunc(DataStore.selectDataSet(data[1]), data[2])
             
             
-    def selectDataByTreeSelection(self):
+    def selectItemTreeSelection(self):
         """
         Using the selected tree item, set the corresponding object 
         in the data store as the current selection.
         """
-        self.applyToSelection(DataStore.selectDataSet, FacsData.selectClustering)
+        item = self.getSanitizedItemSelectionData()
+        
+        if item is not None:
+            if item[0] is DATA_SET_ITEM:
+                self.applyToSelection(DataStore.selectDataSet, FacsData.selectClustering)
+            
+            if item[0] is FIGURE_SET_ITEM:
+                if item[1] != FigureStore.getSelectedIndex():
+                    FigureStore.setSelectedFigure(item[1])
+                    switchFigures(self.Parent.TopLevelParent.facsPlotPanel, FigureStore.getSelectedFigure(), True)
         
     
     def displayDataInfo(self):
-        dataID, _ = self.getSanitizedItemSelectionData()
-        data = DataStore.get(dataID)
-        dlg = DataInfoDialog(self.Parent, data)
-        dlg.Show()
+        type, id, _ = self.getSanitizedItemSelectionData()
+        if (type is DATA_SET_ITEM):
+            data = DataStore.get(id)
+            dlg = DataInfoDialog(self.Parent, data)
+            dlg.Show()
     
     def plotData(self, plotType):
-        dataID, clusterID = self.getSanitizedItemSelectionData()
+        item = self.getSanitizedItemSelectionData()
+        
+        dataID = item[1]
+        clusterID = None if len(item) < 3 else item[2]
         self.Parent.TopLevelParent.facsPlotPanel.plotData(dataID, clusterID, plotType)
             
     def clearClusteringSelection(self):
-        index = self.getItemSelectionData()
-        DataStore.getData()[index].selectedClustering = None
+        type, id, _ = self.getSanitizedItemSelectionData()
+        DataStore.get(id).selectedClustering = None
         self.updateTree()
     
     def renameItem(self):
         """
         Give a new display name to the currently selected data item
         """
-        dataID, clusterID = self.getSanitizedItemSelectionData()
-        data = DataStore.get(dataID)
-        dlg = EditNameDialog(self.Parent, data.displayname)
-        if dlg.ShowModal() == wx.ID_OK:
-            data.displayname = dlg.Text
-            item = self.tree.GetSelection()
-            item.SetText(dlg.Text)
-            self.tree.RefreshSelected()
-            item.SetHilight(False)
-            item.SetHilight(True)
+        item = self.getSanitizedItemSelectionData()
+        if item is None:
+            return
         
-        dlg.Destroy()
+        if (item[0] is DATA_SET_ITEM):
+            data = DataStore.get(item[1])
+            dlg = EditNameDialog(self.Parent, data.displayname)
+            if dlg.ShowModal() == wx.ID_OK:
+                data.displayname = dlg.Text
+            dlg.Destroy()
+                
+        if (item[0] is FIGURE_SET_ITEM):
+            figure = FigureStore.get(item[1])
+            dlg = EditNameDialog(self.Parent, figure.name)
+            if dlg.ShowModal() == wx.ID_OK:
+                figure.name = dlg.Text
+            dlg.Destroy()
+            
+        item = self.tree.GetSelection()
+        item.SetText(dlg.Text)
+        self.tree.RefreshSelected()
+        item.SetHilight(False)
+        item.SetHilight(True)
             
     
     def deleteSelection(self):
         """
         Delete the data object referenced by the current tree selection.
         """
-        self.Parent.TopLevelParent.facsPlotPanel.deleteAssociatedSubplots(self.getSanitizedItemSelectionData())
-        self.applyToSelection(DataStore.remove, FacsData.removeClustering)
-        # if all data deleted, clear axes selectors
-        if len(DataStore.getData()) == 0:
-            self.Parent.TopLevelParent.updateAxesList([])
+        item = self.getSanitizedItemSelectionData()
+        
+        if item is None:
+            return
+        
+        if item[0] is DATA_SET_ITEM:
+            self.Parent.TopLevelParent.facsPlotPanel.deleteAssociatedSubplots()
+            self.applyToSelection(DataStore.remove, FacsData.removeClustering)
+            # if all data deleted, clear axes selectors
+            if len(DataStore.getData()) == 0:
+                self.Parent.TopLevelParent.updateAxesList([])
+                
+        if item[0] is FIGURE_SET_ITEM:
+            id = item[1]
+            
+            #TODO: figure out if this is a good shortcut for clearing the figure of subplots
+            if id == FigureStore.getSelectedIndex():
+                wx.MessageBox('The currently selected Figure cannot be deleted.', 'Invalid Action', wx.OK | wx.ICON_WARNING)
+            else:
+                FigureStore.remove(id)
         
         self.updateTree()
         
-    def setDataExpanded(self, item, flag):
+    def setExpanded(self, item, flag):
+        #item = self.getItemSelectionData()
         data = self.tree.GetItemPyData(item)
-        if (isinstance(data, int)):
-            DataStore.getData()[data].nodeExpanded = flag
-        if (isinstance(data, tuple)):
-            DataStore.getData()[data[0]].infoExpanded[data[1]] = flag
+        
+        if isinstance(data, int):
+            if data is DATA_SET_ITEM:
+                self.dataTreeExpanded = flag
+            if data is FIGURE_SET_ITEM:
+                self.figureTreeExpanded = flag
+        
+        if isinstance(data, tuple):
+            if data[0] is DATA_SET_ITEM:
+                if len(data) == 2:
+                    DataStore.getData()[data[1]].nodeExpanded = flag
+                
+                if len(data) > 3:
+                    DataStore.getData()[data[1]].infoExpanded[data[2]] = flag
 
 
     # Tree Event Handling
@@ -206,23 +306,30 @@ class FacsTreeCtrlPanel(wx.Panel):
         item, flags = self.tree.HitTest(pt)
         if item:
             self.tree.SelectItem(item)
-            self.selectDataByTreeSelection()
+            self.selectItemTreeSelection()
+            
             self.updateTree()
         event.Skip()
 
 
+    # Handle context menus
     def OnRightUp(self, event):
         pt = event.GetPosition();
         item, flags = self.tree.HitTest(pt)
         if item:
             self.tree.SelectItem(item)
-            data = self.getItemSelectionData()
-            if (isinstance(data, tuple)):
-                self.Parent.PopupMenu(TreePopupMenu(self, dataItem=False), pt)
-            elif (isinstance(data, int)):
-                self.Parent.PopupMenu(TreePopupMenu(self, dataItem=True), pt)
-            else:
-                pass
+            item = self.getSanitizedItemSelectionData()
+            if item is None:
+                return
+            
+            if item[0] is DATA_SET_ITEM:
+                if len(item) == 2:
+                    self.Parent.PopupMenu(DataTreePopupMenu(self, dataItem=True), pt)
+                elif len(item) == 3:
+                    self.Parent.PopupMenu(DataTreePopupMenu(self, dataItem=False), pt)
+            
+            if item[1] is FIGURE_SET_ITEM:
+                self.Parent.PopupMenu(FigureTreePopupMenu(self), pt) 
 
 
     def OnSize(self, event):
@@ -230,12 +337,28 @@ class FacsTreeCtrlPanel(wx.Panel):
         self.tree.SetDimensions(0, 0, w, h)
 
 
+    #TODO: it may not be necessary to do this check
     def OnItemExpanded(self, event):
         item = event.GetItem()
         if item:
-            self.setDataExpanded(item, True)
+            self.setExpanded(item, True)
 
     def OnItemCollapsed(self, event):
         item = event.GetItem()
         if item:
-            self.setDataExpanded(item, False)
+            self.setExpanded(item, False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
