1adc306db5898c89ca08a9cd3d9a1b62224f5020
Shareef Dabdoub <shareef@dabdoub.net>
2010-11-05 21:16:25 +0000

    Partially implemented module. Using PCA for FC data isn't straightforward, so for now this will serve as a code example for plugin authors.

--- find/plugins/analysis/pca.py
+++ find/plugins/analysis/pca.py
@@ -1,5 +1,35 @@
 '''
-Created on Jul 30, 2009
+Currently this module is non-functional and serves only as a code
+example for developers of Analysis plugins.
 
-@author: shareef
+@author: Shareef Dabdoub
 '''
+__all__ = ['pca_register']
+
+from matplotlib.mlab import PCA
+import wx
+
+def pca(data, **kwargs):
+    """
+    pca; PCA; Principle Components Analysis
+    """
+    win = None
+    if 'parentWindow' in kwargs:
+        win = kwargs['parentWindow']
+    
+    
+    # if no window available, then something else is calling->return results
+    res = None #call pca
+    
+    return (res, 'PCA completed')
+    
+    
+class PCA_Display(wx.Dialog):
+    def __init__(self, parent, data):
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, 'Principle Components Analysis', size=(250, 150))
+        self.CenterOnParent()
+        
+
+
+def pca_register():
+    return (pca, True)
