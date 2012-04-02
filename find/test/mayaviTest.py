63dab19708bb3635da89e76b4b2f8279d7f07c0b
Shareef Dabdoub <shareef@dabdoub.net>
2012-04-02 14:24:14 +0000

    not really necessary to have under version control

--- find/test/mayaviTest.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def V(x, y, z):
-    """ A 3D sinusoidal lattice with a parabolic confinement. """
-    return np.cos(10*x) + np.cos(10*y) + np.cos(10*z) + 2*(x**2 + y**2 + z**2)
-
-X, Y, Z = np.mgrid[-2:2:100j, -2:2:100j, -2:2:100j]
-Q = V(X, Y, Z)
